48dcae4509135641ce1349d2e1eca14dd46d304a
moazreda0 <moazreda0@gmail.com>
2024-11-24 21:10:32 +0200

    Fix path issue and update project

--- src/Components/Redister/Register.jsx
+++ src/Components/Redister/Register.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useFormik } from "formik";
 import React, { useState } from "react";
+import { Helmet } from "react-helmet";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
